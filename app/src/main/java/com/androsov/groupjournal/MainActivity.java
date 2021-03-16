1da17d833e8a987554b72a7025621b8f33d54b96
Ilya Androsov <ilya.androsau@softensity.com>
2021-03-16 23:50:32 +0300

    init project

---
+++ app/src/main/java/com/androsov/groupjournal/MainActivity.java
@@ -0,0 +1,24 @@
+package com.androsov.groupjournal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void btnLoginClick(View view) {
+        TextView txt = findViewById(R.id.login_title);
+        txt.setText("Other text");
+    }
+
+    //TODO: app
+}
